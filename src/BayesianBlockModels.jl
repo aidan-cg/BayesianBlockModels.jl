3dd18f44d5100b41fb90c7824e57518487e07f91
Aidan Gleich <aidan.gleich@duke.edu>
2024-12-11 12:36:55 -0600

    Files generated by PkgTemplates
    PkgTemplates version: 0.7.53

---
+++ src/BayesianBlockModels.jl
@@ -0,0 +1,5 @@
+module BayesianBlockModels
+
+# Write your package code here.
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using BayesianBlockModels
+using Test
+
+@testset "BayesianBlockModels.jl" begin
+    # Write your tests here.
+end
