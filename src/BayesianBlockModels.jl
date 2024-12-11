00944dcf5a2f8036d9dd7e67ba1ed0e169be7772
Aidan Gleich <aidan.gleich@duke.edu>
2024-12-11 16:07:47 -0600

    update imports and dependencies

--- src/BayesianBlockModels.jl
+++ src/BayesianBlockModels.jl
@@ -1,5 +1,12 @@
 module BayesianBlockModels
 
 # Write your package code here.
+export gibbs_sample
+export sample_groups
+export sample_link_probs
+export sample_theta
 
+include("gibbs.jl")
+include("util.jl")
+include("sample.jl")
 end
